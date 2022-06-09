79c05d1cb0ae6720dd16d02943b515931472f3a8
Plamen Mitev <44363976+o0pmitev@users.noreply.github.com>
2022-06-09 04:35:29 +0000

    Section 2: Working with primitives

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+function add(n1, n2, showResult, resultPhrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(resultPhrase + result);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 3;
+var printResult = true;
+var resultPhrase = "Result is ";
+var result = add(number1, number2, printResult, resultPhrase);
